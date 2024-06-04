673ee0e22809eb89ae574c8a56a4609811648713
monajstha <manojstha700@gmail.com>
2024-06-04 21:59:03 +0545

    add: add playRound conditions to play the game once

--- script.js
+++ script.js
@@ -23,3 +23,38 @@ function getHumanChoice() {
   );
   return humanChoice;
 }
+
+console.log("\n\n\n\n\n\n\n\n\n");
+
+function playRound(human_choice, computer_choice) {
+  if (human_choice === "paper" && computer_choice === "rock") {
+    console.log("You win! Paper beats Rock");
+    human_score++;
+  } else if (human_choice === "rock" && computer_choice === "paper") {
+    console.log("You lose! Paper beats Rock");
+    computer_score++;
+  } else if (human_choice === "scissors" && computer_choice === "paper") {
+    console.log("You win! scissors beats Paper");
+    human_score++;
+  } else if (human_choice === "paper" && computer_choice === "scissors") {
+    console.log("You lose! scissors beats Paper");
+    computer_score++;
+  } else if (human_choice === "rock" && computer_choice === "scissors") {
+    console.log("You win! Rock beats scissors");
+    human_score++;
+  } else if (human_choice === "scissors" && computer_choice === "rock") {
+    console.log("You lose! Rock beats scissors");
+    computer_score++;
+  } else if (human_choice === computer_choice) {
+    console.log(
+      "Tie! Both chose",
+      human_choice
+        .charAt(0)
+        .toUpperCase()
+        .concat(human_choice.slice(1, human_choice.length))
+    );
+    tie_count++;
+  } else {
+    return;
+  }
+}
