d57d7dfc3b476284c705ac49c5c5513597905ccc
monajstha <manojstha700@gmail.com>
2024-06-04 21:56:48 +0545

    generate random choice from computer

---
+++ script.js
@@ -0,0 +1,18 @@
+let human_score = 0;
+let computer_score = 0;
+let tie_count = 0;
+
+function getComputerChoice() {
+  let randomNum = Math.floor(Math.random() * 10);
+  let computerChoice = null;
+  if (
+    (randomNum % 2 === 0 && randomNum % 3 === 0) ||
+    (randomNum % 2 !== 0 && randomNum % 3 !== 0)
+  ) {
+    return (computerChoice = "Rock");
+  } else if (randomNum % 2 === 0) {
+    return (computerChoice = "Paper");
+  } else {
+    return (computerChoice = "scissors");
+  }
+}
